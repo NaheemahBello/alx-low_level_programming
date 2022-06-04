272b146de73e5520fda0fae5a8799b2648594c72
NaheemahBello <nimabello009@gmail.com>
2022-06-03 18:53:40 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,8 +16,8 @@ int main(void)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
-			if(digit1 == 8 && digit2 == 9)
-				continue;	
+			if (digit1 == 8 && digit2 == 9)
+			continue;	
 			putchar(',');
 			putchar(' ');
 		}
