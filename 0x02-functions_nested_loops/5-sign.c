1ab6f4d4c2baaa6c9e46b7abcad1a5adcaa75b40
NaheemahBello <nimabello009@gmail.com>
2022-06-07 17:09:24 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,15 +9,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
-	} else if (n == 0)
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
 	{
 		_putchar(48);
 		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
 	}
-	return (-1);
+	_putchar('\n');
 }
