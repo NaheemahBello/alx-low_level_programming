46ae290f1e09a0471603064d643d1b9173e22f3a
NaheemahBello <nimabello009@gmail.com>
2022-10-06 11:47:15 -0700

    Corrections

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/NaheemahBello/alx-low_level_programming/master/0x17-dynamic_libraries/preload.so
-export LD_PRELOAD=/tmp/preload.so
+wget -P /tmp https://github.com/Joshdb-18/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
